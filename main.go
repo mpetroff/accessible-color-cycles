1d7d21331d6a370703ca3993a98b346fef4837bf
Matthew Petroff <matthew@mpetroff.net>
2018-10-13 18:04:48 -0400

    Add order picking.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -28,14 +29,19 @@ var (
 var decoder = schema.NewDecoder()
 
 type ColorSetQuestion struct {
-	Set1 []string
-	Set2 []string
+	Set1     []string
+	Set2     []string
+	Orders   []string
+	DrawMode int
 }
 
 type ColorSetResponse struct {
-	Set1 string
-	Set2 string
-	Pick int8
+	Set1      string
+	Set2      string
+	Orders    string
+	DrawMode  int
+	SetPick   int8
+	OrderPick int8
 }
 
 func read_colors_csv(filename string) [][]string {
@@ -89,21 +95,23 @@ func post_color(w http.ResponseWriter, r *http.Request) {
 		return
 	}*/
 
-	flashes := session.Flashes()
-	// Randomly generate two color cycles (random sets + random orders)
-	// TODO: should we sometimes use two different orders for the same set?
+	// Randomly pick two color sets
 	cycle1 := append([]string(nil), color_sets[rand.Int31n(len_color_sets)]...)
 	cycle2 := append([]string(nil), color_sets[rand.Int31n(len_color_sets)]...)
-	/*color_idx := rand.Int31n(len_color_sets)
-	cycle1 := append([]string(nil), color_sets[color_idx]...)
-	cycle2 := append([]string(nil), color_sets[color_idx]...)
-	rand.Shuffle(len(cycle1), func(i, j int) {
-		cycle1[i], cycle1[j] = cycle1[j], cycle1[i]
-	})
-	rand.Shuffle(len(cycle2), func(i, j int) {
-		cycle2[i], cycle2[j] = cycle2[j], cycle2[i]
-	})*/
-	session.AddFlash(strings.Join(cycle1, ",") + ";" + strings.Join(cycle2, ","))
+
+	// Randomly generate four permutations
+	orders := [][]int{rand.Perm(8), rand.Perm(8), rand.Perm(8), rand.Perm(8)}
+	var ordersStr []string
+	for _, x := range orders {
+		ordersStr = append(ordersStr, strings.Trim(strings.Replace(fmt.Sprint(x), " ", "", -1), "[]"))
+	}
+
+	// Randomly pick a drawing mode
+	drawMode := rand.Intn(4)
+
+	// Save generated sets, permutations, and drawing mode in session
+	flashes := session.Flashes()
+	session.AddFlash(strings.Join(cycle1, ",") + ";" + strings.Join(cycle2, ",") + ";" + strings.Join(ordersStr, ",") + ";" + strconv.Itoa(drawMode))
 	session.Save(r, w)
 
 	// Parse, verify, and record response
@@ -119,18 +127,20 @@ func post_color(w http.ResponseWriter, r *http.Request) {
 			log.Println(err)
 			return
 		}
-		if flashes[0] != csr.Set1+";"+csr.Set2 {
-			log.Printf("Bad match %s %s\n", flashes[0], csr.Set1+";"+csr.Set2)
+		if flashes[0] != csr.Set1+";"+csr.Set2+";"+csr.Orders+";"+strconv.Itoa(csr.DrawMode) {
+			log.Printf("Bad match %s %s\n", flashes[0], csr.Set1+";"+csr.Set2+";"+csr.Orders+";"+strconv.Itoa(csr.DrawMode))
 		} else {
 			//log.Printf("Good match %s %s\n", flashes[0], csr.Set1 + ";" + csr.Set2)
 			//log.Println("Pick", csr.Pick)
 			zap.L().Info("pick", zap.String("c1", csr.Set1),
-				zap.String("c2", csr.Set2), zap.Int8("p", csr.Pick))
+				zap.String("c2", csr.Set2), zap.String("o", csr.Orders),
+				zap.Int("dm", csr.DrawMode), zap.Int8("sp", csr.SetPick),
+				zap.Int8("cp", csr.OrderPick))
 		}
 	}
 
 	// Encode JSON response with color cycles
-	csq := ColorSetQuestion{cycle1, cycle2}
+	csq := ColorSetQuestion{cycle1, cycle2, ordersStr, drawMode}
 	w.Header().Set("Content-Type", "text/json; charset=utf-8")
 	if err := json.NewEncoder(w).Encode(csq); err != nil {
 		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
@@ -139,25 +149,25 @@ func post_color(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    // Seed PRNG with current time
+	// Seed PRNG with current time
 	rand.Seed(time.Now().UnixNano())
 
-    // Set up logger to record results
-    logger := zap.New(zapcore.NewCore(
-        zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+	// Set up logger to record results
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
 			MessageKey: "type",
 			TimeKey:    "ts",
 			EncodeTime: zapcore.EpochTimeEncoder,
 		}),
-        zapcore.AddSync(&lumberjack.Logger{
-            Filename: "results.log",
-        }),
-        zapcore.InfoLevel,
-    ))
+		zapcore.AddSync(&lumberjack.Logger{
+			Filename: "results.log",
+		}),
+		zapcore.InfoLevel,
+	))
 	defer logger.Sync() // Flushes buffer, if any
 	zap.ReplaceGlobals(logger)
 
-    // Configure web server
+	// Configure web server
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/post_color", post_color)
