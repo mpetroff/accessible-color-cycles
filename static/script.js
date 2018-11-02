mpetroff/accessible-color-cycles

commit ac33e3e9357965328e2dfae04585dbfc8a231f26

Add loading feedback.

static/script.js
@@ -13,6 +13,13 @@ const introductionDiv = document.querySelector('#introduction'),
     numPicksDiv = document.querySelector('#numPicks'),
     messageElem = document.querySelector('#message');
 
+const pickCycleDivs = [
+    document.querySelector('#pickcycle1'),
+    document.querySelector('#pickcycle2'),
+    document.querySelector('#pickcycle3'),
+    document.querySelector('#pickcycle4')
+];
+
 // Handle enabling / disable colorblindness type question in questionnaire
 document.addEventListener('DOMContentLoaded', function() {
     const colorblindnessSelect = document.querySelector('#colorblindnessSelect'),
@@ -79,6 +86,10 @@ function submit(orderPick) {
             }
             directionsDiv.style.display = 'none';
             cyclesDiv.style.display = 'none';
+            for (let j = 0; j < 4; j++) {
+                pickCycleDivs[j].removeAttribute('disabled');
+                pickCycleDivs[j].classList.remove('is-loading');
+            }
             setsDiv.style.display = 'inline';
             numPicksDiv.textContent = xhr.response.Picks;
             picksDiv.style.display = 'inline';
@@ -163,9 +174,12 @@ for (let i = 1; i <= 2; i++) {
         cycles(i);
     });
 }
-for (let i = 1; i <= 4; i++) {
-    document.querySelector('#pickcycle' + i).addEventListener('click', e => {
+for (let i = 0; i < 4; i++) {
+    pickCycleDivs[i].addEventListener('click', e => {
         e.preventDefault();
+        e.target.classList.add('is-loading');
+        for (let j = 0; j < 4; j++)
+            pickCycleDivs[j].setAttribute('disabled', true);
         submit(i);
     });
 }
@@ -188,6 +202,7 @@ document.querySelector('#submitAnswers').addEventListener('click', e => {
 });
 document.querySelector('#directionsRead').addEventListener('click', e => {
     e.preventDefault();
+    e.target.classList.add('is-loading');
     submit(-2);
 });
 

static/style.css
@@ -65,3 +65,35 @@ canvas {
 .message-body {
   padding: 0.5em 1em;
 }
+
+/* uncss:ignore start */
+.card-footer-item.is-loading {
+  position: relative;
+  pointer-events: none;
+  opacity: 0.5;
+  visibility: hidden;
+}
+.card-footer-item.is-loading:after {
+  animation: spinAround 500ms infinite linear;
+  border: 2px solid rgba(0, 0, 0, 0.7);
+  border-radius: 290486px;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  content: "";
+  display: block;
+  position: absolute;
+  top: calc(50% - 0.5em);
+  left: calc(50% - 0.5em);
+  width: 1em;
+  height: 1em;
+  visibility: visible;
+}
+.card-footer-item[disabled] {
+  pointer-events: none;
+  opacity: 0.5;
+}
+.button.is-loading:after {
+    left: calc(50% - 0.5em);
+    top: calc(50% - 0.5em);
+}
+/* uncss:ignore end */

commit d397bc13fccafe0dadd9130570269b0deb8d40fe

Fix regression with cycle picks introduced yesterday in ac33e3e9357965328e2dfae04585dbfc8a231f26.

static/script.js
@@ -180,7 +180,7 @@ for (let i = 0; i < 4; i++) {
         e.target.classList.add('is-loading');
         for (let j = 0; j < 4; j++)
             pickCycleDivs[j].setAttribute('disabled', true);
-        submit(i);
+        submit(i + 1);
     });
 }
 